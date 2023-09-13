143e2fd5da2bb68f069178c5e32da8e3ea1755e8
MahmoudHousam <mahmoudhousam@gmail.com>
2023-09-12 17:19:08 -0700

    betty styling

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * main - print _putchare
  * Return: means success
  */
+int main(void)
+{
+	char message[] = "_putchar";
 
-int main(void){  
-  char message[] = "_putchar";
-  int i = 0;
-  while (i < 8){
-    _putchar(message[i]);
-    ++i;
-  }
-  _putchar('\n');
-  return 0;
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(message[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
