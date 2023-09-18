19834a95ba1974a1dd0cd2af8fce9679ce99a21f
MahmoudHousam <mahmoudhousam60@gmail.com>
2023-09-19 01:58:25 +0300

    testing task 2

--- 0x05-pointers_arrays_strings(testing_version)/main.c
+++ 0x05-pointers_arrays_strings(testing_version)/main.c
@@ -1,13 +1,37 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
+    // 0-reset_to_98
     int n;
 
     n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
+
+    // 1-swap
+    int a;
+    int b;
+
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+
+    // 2-strlen
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
     return (0);
 }
