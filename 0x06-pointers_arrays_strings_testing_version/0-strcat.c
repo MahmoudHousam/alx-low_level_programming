70ff9ae9cb55b421aaa8827beccbf6ff11a07338
MahmoudHousam <mahmoudhousam60@gmail.com>
2023-10-08 01:44:51 +0300

    task 0

---
+++ 0x06-pointers_arrays_strings_testing_version/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src)
+{
+    int i = 0;
+    int k = 0;
+
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    while (src[k] != '\0')
+    {
+        dest[i + k] = src[k];
+        k++;
+    }
+    i++;
+    return (dest);
+}
